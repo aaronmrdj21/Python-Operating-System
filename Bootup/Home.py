e520a8c7daceef90d1b895afea4fad9f92c4c460
aaronmrdj21 <aaronossei@gmail.com>
2025-09-21 19:50:18 +0100

    Removed Red Exit button and made 500x500 window instead

--- Bootup/Home.py
+++ Bootup/Home.py
@@ -6,12 +6,11 @@ pygame.init()
 # Game Window
 win_width = 500
 win_height = 500
-win = pygame.display.set_mode((win_width, win_height), pygame.FULLSCREEN)
+win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("Operating System Simulator")
 run = True
 
 Login.show_login_screen(win)
-exit_button_rect = pygame.Rect(700, 0, 30, 20)
 
 while run:
     for event in pygame.event.get():
@@ -19,10 +18,7 @@ while run:
             run = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if exit_button_rect.collidepoint(event.pos):
                 # Exits fullscreen mode and closes the window
                 run = False
-        
     win.fill((0, 0,255))
-    pygame.draw.rect(win, (255, 0, 0), exit_button_rect)
     pygame.display.update()
