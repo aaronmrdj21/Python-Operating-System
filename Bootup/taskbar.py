a7abac61b24f36706c4b6a0c0da0e7cdb4ab802e
aaronmrdj21 <aaronossei@gmail.com>
2025-09-28 13:25:57 +0100

    Added more Icons and Added Current date varaiable

--- Bootup/taskbar.py
+++ Bootup/taskbar.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-
+import datetime
 
 def show_taskbar(screen):
     # Colors
@@ -13,11 +13,16 @@ def show_taskbar(screen):
 
     # Taskbar dimensions
     taskbar_height = 40
+    
+    today = datetime.datetime.now()
+    today = today.strftime("%B %d, %Y")
+
+    color = (255, 255, 255)
+    smallfont = pygame.font.SysFont('Corbel', 15)
+    date = smallfont.render(today, True, color)
 
     # Icon dimensions and position
     icon_size = 30
-    icon_x = 10
-    icon_y = (taskbar_height - icon_size) // 2
 
     while True:
         for ev in pygame.event.get():
@@ -27,8 +32,11 @@ def show_taskbar(screen):
 
         pygame.draw.rect(screen, taskbar_color, [0, height - taskbar_height, width, taskbar_height])
 
-        pygame.draw.circle(screen, icon_color, (icon_x + icon_size // 2, height - taskbar_height + icon_y + icon_size // 2), icon_size // 2)
-        pygame.draw.circle(screen, icon_color, (icon_x + 70, height - taskbar_height + icon_y + icon_size // 2), icon_size // 2)
-        pygame.draw.rect(screen, icon_color, [icon_x + 100, height - taskbar_height + icon_y, icon_size, icon_size])
-
+        pygame.draw.circle(screen, icon_color, (20, 480), icon_size // 2)
+        pygame.draw.circle(screen, (255,255,0), (60, 480), icon_size // 2)
+        pygame.draw.rect(screen, icon_color, (80, 465, icon_size, icon_size))
+        pygame.draw.circle(screen, (255,0,0), (130, 480), icon_size // 2)
+        pygame.draw.circle(screen, (0,255,0), (170, 480), icon_size // 2)
+        screen.blit(date, (380, 470))
+        # screen.blit(Date, (130, 24 - 40 + 45))
         pygame.display.update()
